8c96e2042d928f88960858985853da5d67bb3042
Justin LaPolla <j.a.lapolla@gmail.com>
2015-10-24 05:11:00 +0000

    Modernize httpd/Makefile and update to v2.4.17
    - Refactor httpd/Makefile to match current best practices.
    - Update Apache HTTP Server version from 2.4.16 to 2.4.17.

--- test/httpd.sh
+++ test/httpd.sh
@@ -6,19 +6,19 @@ d=apache/httpd/
 make="make -f test/httpd.mk"
 
 targets=
-targets="$targets 791485A8"
-targets="$targets httpd-v2.4.16-UNVERIFIED.tar.gz"
-targets="$targets httpd-v2.4.16.tar.gz.asc"
-targets="$targets httpd-v2.4.16.tar.gz"
-targets="$targets httpd.tar.gz"
+targets="$targets $d"791485A8
+targets="$targets $d"httpd-v2.4.17-UNVERIFIED.tar.gz
+targets="$targets $d"httpd-v2.4.17.tar.gz.asc
+targets="$targets $d"httpd-v2.4.17.tar.gz
+targets="$targets $d"httpd.tar.gz
 
 $make $d"clean"
 test `find $d -mindepth 1 -maxdepth 1 ! -name '*.swp' | wc -w` = 3
 
 for target in $targets; do
-  ! $make -q $d"$target"
-  $make $d"$target"
-  $make -q $d"$target"
+  ! $make -q $target
+  $make $target
+  $make -q $target
 done
 
 $make $d"clean"
