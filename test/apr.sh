d1bcd6fc02761938e5d05c34cae5755b00325bb6
Justin LaPolla <j.a.lapolla@gmail.com>
2015-10-24 05:23:24 +0000

    Modernize apr/Makefile
    - Apply current best practice to apr/Makefile. This includes expanding
      recipes and functions at compile time.

--- test/apr.sh
+++ test/apr.sh
@@ -6,24 +6,24 @@ d=apache/httpd/apr/
 make="make -f test/apr.mk"
 
 targets=
-targets="$targets 39FF092C"
-targets="$targets apr-v1.5.2-UNVERIFIED.tar.gz"
-targets="$targets apr-v1.5.2.tar.gz.asc"
-targets="$targets apr-v1.5.2.tar.gz"
-targets="$targets apr-util-v1.5.4-UNVERIFIED.tar.gz"
-targets="$targets apr-util-v1.5.4.tar.gz.asc"
-targets="$targets apr-util-v1.5.4.tar.gz"
-targets="$targets apr.tar.gz"
-targets="$targets apr-util.tar.gz"
+targets="$targets $d"39FF092C
+targets="$targets $d"apr-v1.5.2-UNVERIFIED.tar.gz
+targets="$targets $d"apr-v1.5.2.tar.gz.asc
+targets="$targets $d"apr-v1.5.2.tar.gz
+targets="$targets $d"apr-util-v1.5.4-UNVERIFIED.tar.gz
+targets="$targets $d"apr-util-v1.5.4.tar.gz.asc
+targets="$targets $d"apr-util-v1.5.4.tar.gz
+targets="$targets $d"apr.tar.gz
+targets="$targets $d"apr-util.tar.gz
 
 $make $d"clean"
-test `find $d -mindepth 1 -maxdepth 1 ! -name '*.swp' | wc -w` = 2
+test `find $d -type f ! -name '*.swp'| wc -l` = 2
 
 for target in $targets; do
-  ! $make -q $d"$target"
-  $make $d"$target"
-  $make -q $d"$target"
+  ! $make -q $target
+  $make $target
+  $make -q $target
 done
 
 $make $d"clean"
-test `find $d -mindepth 1 -maxdepth 1 ! -name '*.swp' | wc -w` = 2
+test `find $d -type f ! -name '*.swp'| wc -l` = 2
